45ef79e91be0bd05e1722ef580ad6822485db4f0
hades-linux <hmchan10@hotmail.com>
2022-04-17 16:08:00 -0400

    aws network config

--- aws/aws_vpc.ts
+++ aws/aws_vpc.ts
@@ -1,14 +1,31 @@
-/*import {Construct} from "constructs";
-import {ResourceGroup, VirtualNetwork, Subnet} from "@cdktf/provider-azurerm";
-interface VirtualNetworkConstructProps {
-    resourceGroup: ResourceGroup;
-}
+import {Construct} from "constructs";
+import {Vpc, Subnet} from "@cdktf/provider-aws";
 
 export class VirtualNetworkConstruct extends Construct {
-    public readonly aksvirtualNetwork: VirtualNetwork;
+    public readonly aksvirtualNetwork: Vpc;
     public readonly akssubnet1: Subnet;
     public readonly akssubnet2: Subnet;
-    constructor(scope: Construct, name: string, props: VirtualNetworkConstructProps) {
+    constructor(scope: Construct, name: string, ) {
         super(scope, name);
 
-        const {resourceGroup} = props;*/
+        this.aksvirtualNetwork = new Vpc(this, 'aksvirtualNetwork', {
+            cidrBlock: '10.0.0.0/16',
+            tags: JSON.parse(process.env.AWSTAG!)
+        });
+
+        this.akssubnet1 =new Subnet(this, 'akssubnet1', {
+            vpcId: this.aksvirtualNetwork.id,
+            cidrBlock: '10.0.0.0/17',
+            tags: JSON.parse(process.env.AWSTAG!),
+            dependsOn: [this.aksvirtualNetwork]
+        });
+
+        this.akssubnet2 = new Subnet(this, 'akssubnet2', {
+            vpcId: this.aksvirtualNetwork.id,
+            cidrBlock: '10.0.128.0/17',
+            tags: JSON.parse(process.env.AWSTAG!),
+            dependsOn: [this.aksvirtualNetwork]
+        });
+    }
+}
+
