fb05dce9fb848fe73cdce23bb568a4c0a35acbe5
hades-linux <hmchan10@hotmail.com>
2022-04-17 16:09:35 -0400

    local test get env var

--- getenv.sh
+++ getenv.sh
@@ -9,4 +9,4 @@ rd.value) && \
 export DB_HOST=$(cat $STACK_OUTPUT | jq -r .MySQLServerHostname.value) && \
 export $(grep MYSQL_SERVER_ADMIN_USERNAME= dev.env) && export DB_USER=${MYSQL_SERVER_ADMIN_USERNAME}@$(cat $STACK_OUTPUT | jq -r .ContainerRegistryAdminUsername.value) && \
 export $(grep MYSQL_SERVER_ADMIN_PASSWORD= dev.env) && export DB_PASS=${MYSQL_SERVER_ADMIN_PASSWORD} && \
-export $(grep MYSQL_SCHEMA_NAME= dev.env) && export DB_NAME=${MYSQL_SCHEMA_NAME}
+export $(grep MYSQL_SCHEMA_NAME= dev.env) && export DB_NAME=${MYSQL_SCHEMA_NAME} &&\ export DB_PORT=3306
