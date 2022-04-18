eed1d191a9f5a54e112e33fe90d3e7bffdd7858e
KK-Jacker <200084821@stu.vtc.edu.hk>
2022-04-18 18:52:46 -0400

    migrate data to db use

--- insert_data_tosql.sh
+++ insert_data_tosql.sh
@@ -9,14 +9,10 @@ export AZURE_TENANT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalTenantId.v\
 alue) && \
 export AZURE_CLIENT_SECRET=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalPasswo\
 rd.value) && \
-export DB_HOST=$(cat $STACK_OUTPUT | jq -r .MySQLServerHostname.value) && \
-export $(grep MYSQL_SERVER_ADMIN_USERNAME= $dev_env)&& \
-export $(grep MYSQL_SERVER_ADMIN_PASSWORD= $dev_env) && export DB_PASS=${MYSQL_SERVER_ADMIN_PASSWORD}
-export $(grep MYSQL_SCHEMA_NAME= $dev_env) && export DB_NAME=${MYSQL_SCHEMA_NAME}
-export DB_HOST=127.0.0.1 && export DB_PORT=30306 && export DB_USER="root"
-cd pc_donation
-mysql -h 127.0.0.1 -P 30306 -u ${DB_USER} --password=${DB_PASS} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-mysql -h 127.0.0.1 -P 30306 -u ${DB_USER} --password=${DB_PASS} ${DB_NAME} < ./mysql.sql
+export DB_PASS=123qweASD && export DB_HOST=127.0.0.1 && export DB_PORT=30306 && export DB_USER="root" && export DB_NAME="default_schema"
+cd inputsqldata
+mysql -h 127.0.0.1 -P 30306 -u root --password=123qweASD -e "CREATE DATABASE IF NOT EXISTS default_schema;"
+mysql -h 127.0.0.1 -P 30306 -u root --password=123qweASD default_schema < ./mysql.sql
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
