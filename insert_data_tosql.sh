0a9caac4c351202d5708698d950e073505b8a449
hades-linux <hmchan10@hotmail.com>
2022-04-18 22:48:13 -0400

    updated insert data to azure mysql

--- insert_data_tosql.sh
+++ insert_data_tosql.sh
@@ -1,17 +1,15 @@
-echo to insert data to k8s sql, you have to open new terminal and run below commands to port-forward, because port-forward command does not return
-echo kubectl port-forward service/mysql-service 30306:3306
-sleep 10
 STACK_OUTPUT=stack_output.json && \
 export VAULT_URL=$(cat $STACK_OUTPUT | jq -r .KeyVaultUri.value) && \
 export AZURE_CLIENT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalAppId.value) && \
 export AZURE_TENANT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalTenantId.value) && \
 export AZURE_CLIENT_SECRET=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalPassword.value) && \
+export DB_HOST=$(cat $STACK_OUTPUT  | jq -r .MySQLServerHostname.value) && \
 export $(grep MYSQL_SCHEMA_NAME= ./dev.env) && export DB_NAME=${MYSQL_SCHEMA_NAME} && \
 export $(grep MYSQL_SERVER_ADMIN_PASSWORD= ./dev.env) && export DB_PASS=${MYSQL_SERVER_ADMIN_PASSWORD} && \
-export DB_HOST=127.0.0.1 && export DB_PORT=30306 && export DB_USER="root"
+export $(grep MYSQL_SERVER_ADMIN_USERNAME= ./dev.env)&& export $(grep PROJECT_NAME= ./dev.env)  && export export DB_USER=${MYSQL_SERVER_ADMIN_USERNAME}@${PROJECT_NAME}dev && export DB_PORT=3306
 cd pc_donation
-mysql -h 127.0.0.1 -P 30306 -u ${DB_USER} --password=${DB_PASS} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-mysql -h 127.0.0.1 -P 30306 -u ${DB_USER} --password=${DB_PASS} ${DB_NAME} < ./mysql.sql
+mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASS} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASS} ${DB_NAME} < ./mysql.sql
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
