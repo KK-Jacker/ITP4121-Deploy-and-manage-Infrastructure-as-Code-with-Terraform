48c5d11e51dac6f725552a6d0ab3f88426f3e5f8
KK-Jacker <200084821@stu.vtc.edu.hk>
2022-04-18 23:26:43 -0400

    update deploy method

--- deploy_for_k8s_mysql.sh
+++ deploy_for_k8s_mysql.sh
@@ -2,15 +2,12 @@ terraform apply --auto-approve
 terraform output -state=terraform.ITP4121-Project.tfstate -json > stack_output.json
 STACK_OUTPUT=../ITP4121-azure/stack_output.json && \
 export VAULT_URL=$(cat $STACK_OUTPUT | jq -r .KeyVaultUri.value) && \
-export AZURE_CLIENT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalAppId.valu\
-e) && \
-export AZURE_TENANT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalTenantId.v\
-alue) && \
-export AZURE_CLIENT_SECRET=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalPasswo\
-rd.value) && \
-export IMAGE=itp4121project2dev.azurecr.io/itp4121project2-azure:1e76734
+export AZURE_CLIENT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalAppId.value) && \
+export AZURE_TENANT_ID=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalTenantId.value) && \
+export AZURE_CLIENT_SECRET=$(cat $STACK_OUTPUT  | jq -r .ServicePrincipalPassword.value) && \
+export IMAGE=$(cat $STACK_OUTPUT  | jq -r .DockerImageName.value)
 gcloud container clusters get-credentials $(terraform output -raw kubernetes_cluster_name) --region $(terraform output -raw region)
-kubectl create secret generic web-secret --from-literal='port=3306' --from-literal='username=itp4121project' --from-literal='password=123qweASD' --from-literal='host=itp4121project2dev.mysql.database.azure.com' --from-literal='tablename=default_schema'
+kubectl create secret generic web-secret --from-literal='port=3306' --from-literal='username=itp4121project' --from-literal='password=123qweASD' --from-literal='host=mysql-service' --from-literal='tablename=default_schema'
 kubectl create secret docker-registry azurecr-secret --namespace default --docker-username=${AZURE_CLIENT_ID} --docker-password=${AZURE_CLIENT_SECRET} --docker-server=itp4121project2dev.azurecr.io
 kubectl apply -f mysql-deployment.yaml,mysql-service.yaml
 envsubst < web-deployment.yaml | kubectl apply -f -
